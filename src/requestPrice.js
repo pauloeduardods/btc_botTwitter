9eb53bd71333622849bae1fc61ff130a9923ef4f
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-20 17:54:06 -0300

    Add tests to requestPrices

--- src/requestPrice.js
+++ src/requestPrice.js
@@ -26,40 +26,42 @@ function objParams(content, ...params){
 
 async function mysqlTask(sql) {
   if (!sql) return;
-  //const date = moment().format('YYYY-MM-DD HH:mm:ss');
   return new Promise((resolve, reject) => {
     mysql.getConnection((err, conn) => {
-      if (err) return reject(err);
-      conn.query(sql, (error, result) => {
+      if (err) return reject(new Error(err));
+      conn.query(sql,async (error, result) => {
         conn.release();
-        if (error) return reject(error);
-        return resolve(result);
+        if (error) return reject(new Error(error));
+        return resolve(await result);
       });
     });
   });
 }
 
-function requestPrices() {
-  return new Promise(async (resolve) => {
+async function requestPrices(requestResult_TEST) {
+  return new Promise(async (resolve, reject) => {
     const date = moment().format('YYYY-MM-DD HH:mm:ss')
-    const coinKeys = Object.keys(coins)
+    const coinKeys = Object.keys(coins);
+    const result = [];
     const asyncLoop = coinKeys.map(async coinName => {
       const coin = coins[coinName];
-      const content = await request(coin.url, coin.params);
+      const content = await requestResult_TEST ? requestResult_TEST 
+        : await request(coin.url, coin.params);
       const price = objParams(content, ...coin.jsonParams);
       if (!isNaN(price)) {
         const sql = `INSERT INTO price (name, price, datetime) VALUES("${coinName}", "${parseFloat(price).toFixed(2)}", "${date}")`;
         mysqlTask(sql);
-        return sql;
+        return result.push(sql);
       }
     });
     await Promise.all(asyncLoop);
-    return resolve(asyncLoop);
+    //mysql.end((endError) => reject(new Error(endError)));
+    return resolve(result);
   });
 };  
 
 const ola = async () => {
-  //await requestPrices();
+  await requestPrices();
   //'https://api.binance.com/api/v3/avgPrice', {symbol : 'BTCEUR'}
   //'https://api.bitpreco.com/btc-brl/ticker'
   //console.log(await request('https://api.binance.com/api/v3/avgPrice', { symbol: 'BTCEUR' }));
