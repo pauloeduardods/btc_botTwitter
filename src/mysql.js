753070fc515c4545da7ecba33ae54efebce958d3
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-19 22:31:32 -0300

    Refactor

--- src/mysql.js
+++ src/mysql.js
@@ -4,11 +4,12 @@ if(process.env.ENVIRONMENT == "development"){
   require('dotenv/config')
 }
 
-var pool = mysql.createPool({
+const options = {
   host: process.env.BTCBOT_SQLHOST,
   port: process.env.BTCBOT_SQLPORT,
   user: process.env.BTCBOT_SQLUSER,
   password: process.env.BTCBOT_SQLPASSWD,
   database: process.env.BTCBOT_SQLDATABASE
-})
+};
+var pool = mysql.createPool(options);
 exports.pool = pool
--- src/requestPrice.js
+++ src/requestPrice.js
@@ -1,20 +1,20 @@
-const axios = require('axios')
-const moment = require('moment')
-const mysql = require('./mysql').pool
-const coins = require('../coins').coins
-const tools = require('./tools')
+const axios = require('axios');
+const moment = require('moment');
+const mysql = require('./mysql').pool;
+const coins = require('../coins').coins;
+const tools = require('./tools');
 
 async function request(url, params) {
-  return new Promise(async (resolve, reject) => {
+  return new Promise(async (resolve) => {
     const datetime = moment().format('YYYY-MM-DD HH:mm:ss');
     try {
-      return resolve(await axios.get(url, { params: params }));
+      return resolve((await axios.get(url, { params: params })).data);
     } catch (error) {
       tools.write_log(1, 'request', datetime, error);
-      reject(error);
     }
   });
 }
+
 function objParams(content, ...params){
   return params.reduce((acc, cur) => {
     try {
@@ -25,54 +25,45 @@ function objParams(content, ...params){
   }, content);
 }
 
+async function mysqlTask(sql) {
+  if (!sql) return;
+  //const date = moment().format('YYYY-MM-DD HH:mm:ss');
+  return new Promise((resolve, reject) => {
+    mysql.getConnection((err, conn) => {
+      if (err) return reject(err);
+      conn.query(sql, (error, result) => {
+        conn.release();
+        if (error) return reject(error);
+        return resolve(result);
+      });
+    });
+  });
+}
+
 function requestPrices() {
   return new Promise(async (resolve) => {
     const date = moment().format('YYYY-MM-DD HH:mm:ss')
     const coinKeys = Object.keys(coins)
-    const asyncLoop = coinKeys.map(coinName => {
-      try {
-        const coin = coins[coinName];
-        const content = request(coin.url, coin.params);
-        //console.log(content)
-        const price = objParams(content, coin.jsonParams).toFixed(2)
-        const sql = `INSERT INTO price (name, price, datetime) VALUES("${coinName}", "${price}", "${date}")`;
-        insertDatabase(sql);
+    const asyncLoop = coinKeys.map(async coinName => {
+      const coin = coins[coinName];
+      const content = await request(coin.url, coin.params);
+      const price = objParams(content, ...coin.jsonParams);
+      if (!isNaN(price)) {
+        const sql = `INSERT INTO price (name, price, datetime) VALUES("${coinName}", "${parseFloat(price).toFixed(2)}", "${date}")`;
+        mysqlTask(sql);
         return sql;
-      }
-      catch (error) {
-        tools.write_log(1, "errorRP", date, error);
-        return;
       }
     });
     await Promise.all(asyncLoop);
-    console.log(asyncLoop)
     return resolve(asyncLoop);
   });
 };  
 
 const ola = async () => {
-  console.log(await requestPrices())
   await requestPrices();
+  //'https://api.binance.com/api/v3/avgPrice', {symbol : 'BTCEUR'}
+  //'https://api.bitpreco.com/btc-brl/ticker'
+  //console.log(await request('https://api.binance.com/api/v3/avgPrice', { symbol: 'BTCEUR' }));
 }
 ola();
-
-function mysqlTask(sql) {
-  return new Promise((resolve, reject) => {
-    mysql.getConnection((err, conn) => {
-      if (err) tools.write_log(1, 'sqlConnError', err, date)
-      conn.query(sql, (error, result) => {
-        conn.release()
-        if (error) tools.write_log(1, 'sqlQuerryError', error, date)
-        return resolve(result)
-      });
-    });
-  })
-}
-
-async function requestPrice(){
-  return new Promise(async res =>{
-    
-    res(true)
-  })
-}
-module.exports ={ requestPrice, objParams, requestPrices }
+module.exports = { requestPrices, objParams, requestPrices }
