6e2f64942c740963ebba2c026197a65af2a0f2d7
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-10-10 17:31:14 -0300

    Add writeLogFile

---
+++ src/utils/writeLog.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const moment = require('moment')
+const { dirname } = require('path');
+
+const dirPath = dirname(__dirname) + '/data';
+
+function writeLog(type='Logs', fileName='log', ...text) {
+  const path = `${dirPath}/${type}`
+  if (!fs.existsSync(path)) fs.mkdirSync(path, { recursive: true });
+  const finalPath = `${path}/${fileName}.${type.toLowerCase().replace('s', '')}`
+  const stream = fs.createWriteStream(finalPath, { flags: 'a' });
+  stream.write(`${moment().format('YYYY-MM-DD HH:mm:ss')},${text.join(',')}\n`);
+  stream.end();
+}
+
+module.exports = writeLog;
