625b49d81d413bfde4210a56e55340051d8e9911
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-10-10 17:36:50 -0300

    Refactor writelog

--- src/utils/writeLog.js
+++ src/utils/writeLog.js
@@ -4,13 +4,22 @@ const { dirname } = require('path');
 
 const dirPath = dirname(__dirname) + '/data';
 
-function writeLog(type='Logs', fileName='log', ...text) {
-  const path = `${dirPath}/${type}`
+function writeLog(fileName, ...text) {
+  const path = `${dirPath}/Logs`
   if (!fs.existsSync(path)) fs.mkdirSync(path, { recursive: true });
-  const finalPath = `${path}/${fileName}.${type.toLowerCase().replace('s', '')}`
+  const finalPath = `${path}/${fileName}.log`
   const stream = fs.createWriteStream(finalPath, { flags: 'a' });
   stream.write(`${moment().format('YYYY-MM-DD HH:mm:ss')},${text.join(',')}\n`);
   stream.end();
 }
 
-module.exports = writeLog;
+function writeError(fileName, ...text) {
+  const path = `${dirPath}/Errors`
+  if (!fs.existsSync(path)) fs.mkdirSync(path, { recursive: true });
+  const finalPath = `${path}/${fileName}.err`
+  const stream = fs.createWriteStream(finalPath, { flags: 'a' });
+  stream.write(`${moment().format('YYYY-MM-DD HH:mm:ss')},${text.join(',')}\n`);
+  stream.end();
+}
+
+module.exports = { writeLog, writeError };
