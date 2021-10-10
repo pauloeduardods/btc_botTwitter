d335db731e6458ad3fa3f20e637c250f78c90483
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-10-10 17:41:12 -0300

    Update mysql with writeError

--- src/mysql.js
+++ src/mysql.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const moment = require('moment');
-const writeLog = require('./utils/writeLog');
+const { writeError } = require('./utils/writeLog');
 
 if (process.env.ENVIRONMENT == 'development') {
   require('dotenv/config');
@@ -32,12 +32,12 @@ async function mysqlTask(sql) {
 
 const insertPrice = (coinName, price, dateTime) => {
   return new Promise((resolve) => {
-    if (!coinName || !price) return writeLog(1, 'insetPrice-mysql', 'missing params', coinName, price, dateTime);
+    if (!coinName || !price) return writeError('insetPrice-mysql', 'missing params', coinName, price, dateTime);
     const date = dateTime || moment().format('YYYY-MM-DD HH:mm:ss');
     try {
       resolve(mysqlTask(`INSERT INTO price (name, price, datetime) VALUES("${coinName}", "${parseFloat(price).toFixed(2)}", "${date}")`));
     } catch (error) {
-      writeLog(1, 'insetPrice-mysql', error);
+      writeError('insetPrice-mysql', error);
     }
   });
 }
@@ -46,21 +46,14 @@ async function getPrice(exchange, dateTimeStart, dateTimeEnd) {
   return new Promise((resolve) => {
     const startDateTime = dateTimeStart || moment().subtract(30, 'minutes').format('YYYY-MM-DD HH:mm:ss');
     const endDateTime = dateTimeEnd || moment().add(1, 'minutes').format('YYYY-MM-DD HH:mm:ss');
-    if (!exchange) return writeLog(1, 'getPrice-mysql', 'missing params', exchange, dateTimeStart, dateTimeEnd);
+    if (!exchange) return writeError('getPrice-mysql', 'missing params', exchange, dateTimeStart, dateTimeEnd);
     try {
       const sql = `SELECT price FROM price WHERE name = "${exchange}" AND datetime >= "${startDateTime}" AND datetime <= "${endDateTime}"`;
       resolve(mysqlTask(sql));
     } catch (error) {
-      writeLog(1, 'getPrice-mysql', error);
+      writeError('getPrice-mysql', error);
     }
   })
 }
-
-async function sla() {
-  await insertPrice('teste', 29.73);
-  const result = await getPrice('teste');
-  console.log(result[result.length - 1].price);
-}
-sla()
 
 module.exports = { getPrice, insertPrice };
