9ec0887d69566a3e33b247e2e84a0c6f7fded9a1
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-10-10 15:21:48 -0300

    Refactor tools.js

--- src/tools.js
+++ src/tools.js
@@ -1,18 +1,19 @@
-const fs = require('fs')
-const {} = require('./mysql')
-const moment = require('moment')
-const { dirname } = require('path')
+const fs = require('fs');
+// const {} = require('./mysql')
+// const moment = require('moment')
+const { dirname } = require('path');
 
-const path = dirname(__dirname) + '/data'
-const log = path + '/logs'
-const erro = path + '/errors'
-const paths = [log, erro]
-function write_log(path_num = Number, file_name = String, ...text){
-  if(!fs.existsSync(paths[path_num])) fs.mkdirSync(paths[path_num], { recursive: true })
-  var path_ = paths[path_num] + '/' + file_name + '.txt'
-  stream = fs.createWriteStream(path_, {flags:'a'});
-  stream.write(text + '\n')
-  stream.end()
+const dirPath = dirname(__dirname) + '/data';
+const log = dirPath + '/logs';
+const erro = dirPath + '/errors';
+const paths = [log, erro];
+
+function write_log(pathNum = Number, fileName = String, ...text){
+  if (!fs.existsSync(paths[pathNum])) fs.mkdirSync(paths[pathNum], { recursive: true });
+  let path = paths[pathNum] + '/' + fileName + '.txt';
+  const stream = fs.createWriteStream(path, {flags:'a'});
+  stream.write(text + '\n');
+  stream.end();
 }
 
 function format_price(value = Number, locale = 'en-US'){
@@ -21,4 +22,5 @@ function format_price(value = Number, locale = 'en-US'){
   if (locale == 'en-US') return `${Number(value[0]).toLocaleString('en-US')}.${value[1]}`
   return `${Number(value[0]).toLocaleString('pt-BR')},${value[1]}`
 }
-module.exports = {write_log, getPrice, format_price}
+
+module.exports = { write_log, format_price}
