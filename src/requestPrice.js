bd20ccceabcf87402c3cd5a4383152d63b3aa67e
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-10-10 18:34:57 -0300

    Refactor requestPrice

--- src/requestPrice.js
+++ src/requestPrice.js
@@ -1,17 +1,14 @@
 const axios = require('axios');
-const moment = require('moment');
-const { mysqlTask } = require('./mysql');
+const { insertPrice } = require('./mysql');
+const { writeError } = require('./utils/writeLog');
 const coins = require('../coins').coins;
-const tools = require('./tools');
 
 async function request(url, params) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      return resolve((await axios.get(url, { params: params })).data);
-    } catch (error) {
-      return reject(new Error(error));
-    }
-  });
+  try {
+    return (await axios.get(url, { params: params })).data;
+  } catch (error) {
+    writeError('request-requestPrice', error);
+  }
 }
 
 function objParams(content, ...params){
@@ -19,35 +16,24 @@ function objParams(content, ...params){
     try {
       return acc[cur];
     } catch {
-      return;
+      return acc;
     }
   }, content);
 }
 
-async function requestPrices(requestResult_TEST) {
-  return new Promise(async (resolve, reject) => {
-    const date = moment().format('YYYY-MM-DD HH:mm:ss')
-    const coinKeys = Object.keys(coins);
-    const result = [];
-    const asyncLoop = coinKeys.map(async coinName => {
-      const coin = coins[coinName];
-      const content = await requestResult_TEST ? requestResult_TEST 
-        : await request(coin.url, coin.params);
-      const price = objParams(content, ...coin.jsonParams);
-      if (!isNaN(price)) {
-        const sql = `INSERT INTO price (name, price, datetime) VALUES("${coinName}", "${parseFloat(price).toFixed(2)}", "${date}")`;
-        mysqlTask(sql);
-        return result.push(sql);
-      }
-    });
-    await Promise.all(asyncLoop);
-    //mysql.end((endError) => reject(new Error(endError)));
-    return resolve(result);
+async function requestPrices() {
+  const coinKeys = Object.keys(coins);
+  const asyncLoop = coinKeys.map(async (coinName) => {
+    const coin = coins[coinName];
+    const content = await request(coin.url, coin.params);
+    const price = objParams(content, ...coin.jsonParams);
+    if (!isNaN(price)) {
+      insertPrice(coinName, price);
+    }
+    return;
   });
-};  
-
-const ola = async () => {
-  await requestPrices();
+  await Promise.all(asyncLoop);
+  return true;
 }
-//ola();
-module.exports = { requestPrices, objParams, requestPrices, request }
+
+module.exports = { requestPrices, objParams, request }
