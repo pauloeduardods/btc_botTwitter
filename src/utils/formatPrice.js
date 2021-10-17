295b6a6061c3d6b73a068028f1a132829282b3f5
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-10-16 21:07:22 -0300

    Fix issues

--- src/utils/formatPrice.js
+++ src/utils/formatPrice.js
@@ -1,4 +1,5 @@
-function formatPrice(value = Number, locale = 'en-US'){
+function formatPrice(value, locale = 'en-US') {
+  if (isNaN(value)) return;
   value = Number(value).toFixed(2);
   value = String(value).split('.');
   if (locale == 'en-US') return `${Number(value[0]).toLocaleString(locale)}.${value[1]}`;
